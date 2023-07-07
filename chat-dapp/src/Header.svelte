f3c64148da8375281c6b7b3b5b3c98fd9c489694
Divij <ndivij2004@gmail.com>
2023-07-07 19:17:57 +0530

    new commit

---
+++ chat-dapp/src/Header.svelte
@@ -0,0 +1,27 @@
+<script>
+import {username,user} from './user';
+
+function signout(){
+    user.leave();
+    username.set('');
+}
+</script>
+
+<header>
+    <h1>GruWeb3</h1>
+    {#if username}
+
+    <div class="user-bio">
+        <span>Hello <strong>{$username}</strong></span>
+        <img src = {'https://avatars.dicebar.com/api/initials/${$username}.svg'} alt = "avatar"/>
+    </div>
+
+    <button class="signout-button" on:click={signout}>
+
+        Sign Out
+    
+    
+    </button>
+
+    {/if}
+</header>
